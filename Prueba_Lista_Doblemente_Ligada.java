13b185fbb9ef3d881fa60b69e3747b70216cc3b0
Rojas Perez Cesar Emiliano <cesarrojas60@aragon.unam.mx>
2022-09-23 00:09:44 -0500

    Lista Doblemente Ligada

---
+++ Prueba_Lista_Doblemente_Ligada.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Lista;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Prueba_Lista_Doblemente_Ligada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LDobleLig<Integer> j = new LDobleLig();
+        j.agregarAlFinal(100);
+        j.agregarAlFinal(50);
+        j.agregarAlFinal(5);
+        j.agregarAlFinal(56);
+        j.agregarAlFinal(80);
+        j.transversal();
+        j.agregarAlInicio(12);
+        j.transversal();
+        j.agregarDespuesDe(7,2);
+        j.transversal();
+        j.eliminar(3);
+        j.transversal();
+        j.eliminarElPrimero();
+        j.transversal();
+        j.eliminarElFinal();
+        j.transversal();
+        System.out.println(j.buscar(56));
+        j.actualizar(50, 8);
+        j.transversal();
+    }
+}
+    
+
