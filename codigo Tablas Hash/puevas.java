15e469a34b2598e0f6d2750a69456e39f9b74521
Rojas Perez Cesar Emiliano <cesarrojas60@aragon.unam.mx>
2022-11-18 03:19:23 -0600

    Tablas Hash, expuesto a cambios

---
+++ codigo Tablas Hash/puevas.java
@@ -0,0 +1,27 @@
+package tablashash;
+
+
+public class puevas {
+    public static void main(String[] args) {
+        TablasHash numeros =new TablasHash(9);
+        numeros.Add("HOLA MUNDO ");
+        numeros.Add("HOLA MUNDO ");
+        numeros.Add("JOse ");
+        numeros.Add("Maria ");
+        numeros.Add("Morelos ");
+        numeros.Add("Pavon ");
+        numeros.Add("Ana ");
+        numeros.Add("Betillo");
+        numeros.Add("cacahuetes ");
+        numeros.Add("Francisco");
+       // System.out.println(numeros);
+     //  numeros.Remove(72);
+       //numeros.Remove(72);
+       //numeros.Remove(72);
+        numeros.Remove(97);
+        System.out.println("/////");
+        System.out.println(numeros);
+    }
+       
+    
+}
